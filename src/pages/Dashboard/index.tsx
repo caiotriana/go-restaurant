0cc4a6b4dd6a8ffe3fdafd433cad828340b97be0
caio <caio_triana@hotmail.com>
2021-06-28 01:06:58 -0300

    create a types for food and transform dashboard page to typescript

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -7,12 +7,14 @@ import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
 import { FoodsContainer } from './styles';
 
+import { FoodTypes } from './dashboard.types';
+
 
 export default function Dashboard() {
-  const [ foods, setFoods ] = useState([])
-  const [ editingFood, setEditingFood ] = useState({})
-  const [ modalOpen, setModalOpen ] = useState(false)
-  const [ editModalOpen, setEditModalOpen ] = useState(false)
+  const [ foods, setFoods ] = useState<FoodTypes[]>([])
+  const [ editingFood, setEditingFood ] = useState<any>({})
+  const [ modalOpen, setModalOpen ] = useState<boolean>(false)
+  const [ editModalOpen, setEditModalOpen ] = useState<boolean>(false)
 
   const getFoods = async () => {
     const response = await api.get('/foods');
@@ -23,7 +25,7 @@ export default function Dashboard() {
     getFoods()
   }, [])
 
-  const handleAddFood = async food => {
+  const handleAddFood = async (food: FoodTypes ) => {
     try {
       const response = await api.post('/foods', {
         ...food,
@@ -37,7 +39,7 @@ export default function Dashboard() {
     }
   }
 
-  const handleDeleteFood = async id => {
+  const handleDeleteFood = async (id: number )=> {
     await api.delete(`/foods/${id}`);
 
     const foodsFiltered = foods.filter(food => food.id !== id);
@@ -45,7 +47,7 @@ export default function Dashboard() {
     setFoods(foodsFiltered);
   }
 
-  const handleUpdateFood = async food => {
+  const handleUpdateFood = async (food: FoodTypes ) => {
     try {
       const foodUpdated = await api.put(
         `/foods/${editingFood.id}`,
@@ -70,7 +72,7 @@ export default function Dashboard() {
     setEditModalOpen(!editModalOpen);
   }
 
-  const handleEditFood = food => {
+  const handleEditFood = (food: FoodTypes ) => {
     setEditingFood(food);
     setEditModalOpen(true);
   }
